4cd4925bfc4d8625fbcf6e1ea6826f0bc98a8093
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-11-24 00:42:39 +0530

    order apis manage

--- src/State/store.js
+++ src/State/store.js
@@ -3,12 +3,14 @@ import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
 import { customerProductReducer } from "./Product/Reducer";
 import { cartReducer } from "./Cart/Reducer";
+import { orderReducer } from "./Order/Reducer";
 
 
 const rootReducers = combineReducers({
     auth:authReducer,
     product:customerProductReducer,
     cart:cartReducer,
+    order:orderReducer
 });
 
 
