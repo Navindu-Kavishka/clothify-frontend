f9e436bf1b0bbcc3acd7f092e8c64bc6ebd3d05d
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-11-23 23:37:28 +0530

    product apis manage

--- src/State/store.js
+++ src/State/store.js
@@ -1,10 +1,14 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
+import { customerProductReducer } from "./Product/Reducer";
+import { cartReducer } from "./Cart/Reducer";
 
 
 const rootReducers = combineReducers({
-    auth:authReducer
+    auth:authReducer,
+    product:customerProductReducer,
+    cart:cartReducer,
 });
 
 
